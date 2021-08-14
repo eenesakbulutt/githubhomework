53d74128950b2d26e052158ec1074e77aa0d4e7d
Enes Akbulut <eenesakbulutt@gmail.com>
2021-08-14 18:06:18 -0400

    Homework done

---
+++ githubhomework.java
@@ -0,0 +1,19 @@
+package githubhomework;
+
+public class githubhomework {
+
+	public static void main(String[] args) {
+		
+		
+		
+	for(int i=1; i<11;i++) {
+		System.out.print(i+" ");
+	}
+		
+		
+		
+		
+		
+	}
+
+}
